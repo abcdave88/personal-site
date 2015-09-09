6cc5d4f37896d384090c41c2aea3ddaa801ea2df
David James <d.james88@btinternet.com>
2015-09-09 17:12:03 +0100

    progress made

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-console.log('Hey')
+console.log('hello world')
+
+$(document).ready(function(){
+
+$('#jumbo2').hide();
+$('#jumbo3').hide();
+
+function changeJumbo(){
+ 
+  $('#jumbo1').delay(3000).fadeOut(1500, function(){
+      $('#jumbo2').fadeIn(1500).delay(3000).fadeOut(1500, function(){
+        $('#jumbo3').fadeIn(1500).delay(3000).fadeOut(1500, function(){
+          $('#jumbo1').fadeIn(1500, function(){
+              var i = 0;
+              if (i < 1){
+                changeJumbo();
+              }
+          });
+        });
+      });
+  });
+}
+changeJumbo();
+
+
+});//end of document.ready
